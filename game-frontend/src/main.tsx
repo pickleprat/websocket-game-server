0bb5de94aa23bcb26f6629dacc68efcaa02ce759
pickleprat <prat16here@gmail.com>
2025-03-15 16:39:30 +0530

    Adding the frontend login page

--- game-frontend/src/main.tsx
+++ game-frontend/src/main.tsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router' 
+import { AuthContext } from './AuthContext'
 
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={ router }/>
+    <AuthContext.Provider value={ undefined }>
+      <RouterProvider router={ router }/>
+    </AuthContext.Provider> 
   </StrictMode>,
 )
