26cbda1f8e1039cfa17a6ae8ae38b1e6df1f0880
pickleprat <prat16here@gmail.com>
2025-04-03 17:53:10 +0530

    Added some more code

--- game-frontend/src/components/Rooms/LiveRooms.tsx
+++ game-frontend/src/components/Rooms/LiveRooms.tsx
@@ -1,6 +1,9 @@
 import { JSX  } from "react";
+import { useParams } from "react-router-dom";
 
 export default function LiveRoom(): JSX.Element {
+    const { roomId } = useParams(); 
+    console.log(roomId); 
     return (
         <>
         </>
