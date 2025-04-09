a8dd6cc0665a2e16859e0c01368b9ca291ff29a2
pickleprat <prat16here@gmail.com>
2025-04-09 22:42:30 +0530

    websocket server not connecting to api

--- game-frontend/src/components/Rooms/LiveRooms.tsx
+++ game-frontend/src/components/Rooms/LiveRooms.tsx
@@ -1,9 +1,14 @@
 import { JSX  } from "react";
 import { useParams } from "react-router-dom";
+import { useAuthContext } from "../../AuthContext";
+import useWebSocket from "react-use-websocket";
 
 export default function LiveRoom(): JSX.Element {
     const { roomId } = useParams(); 
-    console.log(roomId); 
+    const authSesh = useAuthContext(); 
+    const {sendJsonMessage, lastJsonMessage} = useWebSocket("ws://localhost:8000/api/connectRoom"); 
+    console.log(sendJsonMessage, lastJsonMessage); 
+
     return (
         <>
         </>
